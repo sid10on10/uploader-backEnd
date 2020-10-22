fd5aa22e5218ac46e26742d3e40f19c32f82dc53
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-23 01:09:46 +0530

    heroku support

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ var mongodClient = mongodb.MongoClient;
 // can use process.env.MongoUrl
 //var url = process.env.MongoUrl
 var url = "mongodb+srv://sid10on10:qwerty123@cluster0.fqer8.mongodb.net/uploader?retryWrites=true&w=majority" 
-const project_path = __dirname
 
-module.exports = {url,mongodClient,project_path}
+module.exports = {url,mongodClient}
--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ const base_dir = project_path
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, "./uploads");
+      cb(null, "uploads");
   },
   filename: (req, file, cb) => {
       //console.log(file);
