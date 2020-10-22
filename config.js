a71d1c30a6681b747d9dcbc395e8136a2df5acf0
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-22 23:12:43 +0530

    atlas cluster

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var mongodb = require('mongodb')
 var mongodClient = mongodb.MongoClient;
-var url = process.env.MongoUrl
+// can use process.env.MongoUrl
+var url = "mongodb+srv://sid10on10:qwerty123@cluster0.fqer8.mongodb.net/uploader?retryWrites=true&w=majority" 
 
 
 module.exports = {url,mongodClient}
